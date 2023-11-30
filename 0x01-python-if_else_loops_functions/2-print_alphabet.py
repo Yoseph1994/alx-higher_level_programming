9adde85cb6423b61b2e0f14fab030aa8dc62fb4d
Yoseph1994 <joshrde2002@gmail.com>
2023-11-30 05:50:49 -0800

    fixed issues

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ascii_letters in range(ord('a'), ord('z')+1):
-    print(chr(ascii_letters).format(ascii_letters), end=" ")
+for i in range(97, 123):
+    print(chr(i), end='')
