fe3dc4cc6ed611a61c05dd8c7f5abad82a40dc7d
Yoseph1994 <joshrde2002@gmail.com>
2023-12-04 13:14:47 -0800

    rem

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    my_string=my_string.lower()
-    my_string=[char for char in my_string]
-    while 'c' in my_string:
-        my_string.remove('c')
-        new_string=''.join(my_string)
-        return new_string
