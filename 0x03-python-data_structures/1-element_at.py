98da6852dfb32996ba28eb90a6b78b613e7d7c46
Yoseph1994 <joshrde2002@gmail.com>
2023-12-04 13:08:02 -0800

    dummy bug

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def element_at(my_list, idx, element):
+def element_at(my_list, idx):
     if idx < 0 or idx > len(my_list) - 1:
-        return my_list
+        return None
     else:
-        my_list[idx] = element
-        return my_list
+        return my_list[idx]
