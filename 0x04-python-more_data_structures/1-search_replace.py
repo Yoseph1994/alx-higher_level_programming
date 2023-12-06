667abb32304e310c8a2270752c04f8ad801312a7
Yoseph1994 <joshrde2002@gmail.com>
2023-12-06 02:04:24 -0800

    replacing function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        if search in my_list:
-            idx= my_list.index(search)
-            my_list[idx]=replace
-            new_list=[*my_list]
-            return new_list
+    if search in my_list:
+        new_list=[replace if el==search else el for el in my_list ]
+        return new_list
