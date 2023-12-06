fa73cc43e1121d3e76270297a246d820dcc6ef65
Yoseph1994 <joshrde2002@example.com>
2023-12-06 16:23:09 +0300

    condn check

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,10 +2,9 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    """
-    A function that replaces or adds
-    key/value in a dictionary.
-    """
-    a_dictionary[key] = value
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    else:
+        a_dictionary[key] = value
     return a_dictionary
     
