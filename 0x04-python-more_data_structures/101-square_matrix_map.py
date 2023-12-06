5afd2c1d761616ec953e61920ada09c6dbbffb7b
Yoseph1994 <joshrde2002@example.com>
2023-12-06 19:33:03 +0300

    err

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x**2, row)), matrix))
+"""
+Returns a new matrix with squared values of all integers in the input matrix,
+without modifying the original matrix.
+
+Args:
+matrix: A list of lists representing the 2D matrix.
+
+Returns:
+A new list of lists containing squared values of the original matrix.
+"""
+
+def square_row(row):
+return list(map(lambda x: x\*\*2, row))
+
+return list(map(square_row, matrix))
