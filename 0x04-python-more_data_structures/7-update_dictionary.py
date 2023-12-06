8b068a64b1bde071c06c3d8ca4c0f8e1c0ec5de5
Yoseph1994 <joshrde2002@example.com>
2023-12-06 16:26:44 +0300

    idnt

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 
 def update_dictionary(a_dictionary, key, value):
     if key in a_dictionary:
-        a_dictionary[key] = value
+        a_dictionary[key] = value    
     else:
-        a_dictionary[key] = value
-    return a_dictionary 
+        a_dictionary[key] = value    
+    return a_dictionary
