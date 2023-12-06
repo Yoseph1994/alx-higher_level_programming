d1c193d4540c239b64aa1878cba42811a8144286
Yoseph1994 <joshrde2002@example.com>
2023-12-06 18:27:53 +0300

    del

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-def multiply_by_2(a_dictionary):
-    dict_new = {}
-    for key, val in a_dictionary.items():
-        dict_new.update({key: (val * 2)})
-    print(dict_new)
-    return dict_new
