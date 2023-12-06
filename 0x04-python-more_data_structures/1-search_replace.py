660fcdd6d138f7fa238e6b2fe2efcdec86f1b728
Yoseph1994 <joshrde2002@gmail.com>
2023-12-06 01:51:29 -0800

    search nd replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    if my_list:
+        if search in my_list:
+            idx= my_list.index(search)
+            my_list[idx]=replace
+            new_list=[*my_list]
+            return new_list
