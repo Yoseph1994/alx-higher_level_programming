3bbbde330966c09fc8b20404d2980d0ba9e570eb
Yoseph1994 <joshrde2002@gmail.com>
2023-12-06 02:16:52 -0800

    using for in

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    if search in my_list:
-        new_list=[replace if el==search else el for el in my_list ]
-        return new_list
+def search_replace (my_list,search,replace):
+    new_list=[]
+    for el in my_list:
+        if el == search:
+            new_list.append(replace)
+        else:
+            new_list.append(el)
+    return new_list
