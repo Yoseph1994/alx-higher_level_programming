defff6f4abac7ea07b98ada5340e3553e7c5ea9b
Yoseph Shimelis <joshrde2002@gmail.com>
2024-03-22 21:25:25 +0300

    Add files via upload

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-squares=[]
 def square_matrix_simple(matrix=[]):
-    for nums in matrix:
-        for num in nums:
-            squared=num **2
-            print(squared)
 
-
-        
-        
-
-
-square_matrix_simple([[1,2,3],[4,5,6],[7,8,9]])
+    if matrix:
+        new_matrix = [[element ** 2 for element in submatrix]
+                      for submatrix in matrix]
+        return new_matrix
