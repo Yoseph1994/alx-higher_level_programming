27a1277f71ef378248a27f2bbd1846916cca6184
Yoseph1994 <joshrde2002@example.com>
2024-03-22 21:22:54 +0300

    add

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+squares=[]
+def square_matrix_simple(matrix=[]):
+    for nums in matrix:
+        for num in nums:
+            squared=num **2
+            print(squared)
+
+
+        
+        
+
+
+square_matrix_simple([[1,2,3],[4,5,6],[7,8,9]])
